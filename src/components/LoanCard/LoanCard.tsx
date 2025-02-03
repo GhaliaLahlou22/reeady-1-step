12a3c7ae58a0a4044f1856b7e6392db077c9918e
Ghalia Lahlou <ghalia.lahlou4@etu.univ-lorraine.fr>
2025-02-03 17:10:44 +0100

    validation de forum

--- src/components/LoanCard/LoanCard.tsx
+++ src/components/LoanCard/LoanCard.tsx
@@ -5,6 +5,26 @@ const LoanCard = () => {
   const [interestRates, setInterestRates] = useState(3.5)
   const [loanDuration, setLoanDuration] = useState(1)
 
+  const [errors, setErrors] = useState({
+    loanAmount: '',
+    interestRates: '',
+    loanDuration: ''
+  })
+
+  const validateInput = (name: string, value: number) => {
+    if (value <= 0) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: 'La valeur doit être supérieure à 0'
+      }))
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: ''
+      }))
+    }
+  }
+
   const calculerMensualite = (
     loanAmount: number,
     interestRates: number,
@@ -23,18 +43,27 @@ const LoanCard = () => {
     loanDuration
   ).toFixed(2)
 
+  const hasErrors = Object.values(errors).some((error) => error !== '')
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#12372A]">
-      <div className="max-w-md mx-auto p-6 bg-[#2a8162] rounded-2xl shadow-md space-y-4 text-[#F5C518]">
+    <div className="flex items-center justify-center min-h-screen bg-[#0f2a20]">
+      <div className="max-w-md mx-auto p-6 bg-[#285944] rounded-2xl shadow-md space-y-4 text-[#d7c068]">
         <h2 className="text-xl font-bold text-center">Calcul de mensualité</h2>
         <div>
           <label>Montant du prêt (€): </label>
           <input
             type="number"
             value={loanAmount}
-            onChange={(e) => setLoanAmount(Number(e.target.value))}
-            className="w-full p-1 border rounded bg-transparent text-white border-[#F5C518]"
+            onChange={(e) => {
+              const amountVal = Number(e.target.value)
+              setLoanAmount(amountVal)
+              validateInput('loanAmount', amountVal)
+            }}
+            className="w-full p-1 border rounded bg-transparent text-white border-[#d7c068]"
           ></input>
+          {errors.loanAmount && (
+            <p className="text-red-500">{errors.loanAmount}</p>
+          )}
         </div>
 
         <div>
@@ -42,22 +71,38 @@ const LoanCard = () => {
           <input
             type="number"
             value={interestRates}
-            onChange={(e) => setInterestRates(Number(e.target.value))}
-            className="w-full p-1 border rounded bg-transparent text-white border-[#F5C518]"
+            onChange={(e) => {
+              const interestVal = Number(e.target.value)
+              setInterestRates(interestVal)
+              validateInput('interestRates', interestVal)
+            }}
+            className="w-full p-1 border rounded bg-transparent text-white border-[#d7c068]"
           ></input>
+          {errors.interestRates && (
+            <p className="text-red-500">{errors.interestRates}</p>
+          )}
         </div>
         <div>
           <label>Durée du prêt (Année) : </label>
           <input
             type="number"
             value={loanDuration}
-            onChange={(e) => setLoanDuration(Number(e.target.value))}
-            className="w-full p-1 border rounded bg-transparent text-white border-[#F5C518]"
+            onChange={(e) => {
+              const durationVal = Number(e.target.value)
+              setLoanDuration(durationVal)
+              validateInput('loanDuration', durationVal)
+            }}
+            className="w-full p-1 border rounded bg-transparent text-white border-[#d7c068]"
           ></input>
+          {errors.loanDuration && (
+            <p className="text-red-500">{errors.loanDuration}</p>
+          )}
         </div>
-        <div>
-          <p className="text-center">Mensualité : {monthlyPayment} €</p>
-        </div>
+        {!hasErrors && (
+          <div>
+            <p className="text-center">Mensualité : {monthlyPayment} €</p>
+          </div>
+        )}
       </div>
     </div>
   )
